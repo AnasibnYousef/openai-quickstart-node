f819993e8c468544995e179b58765783d665f16d
Mohammed Anas <anasky111@gmail.com>
2023-01-05 17:19:38 +0530

    Increased response length

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -29,11 +29,9 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(query),
+      max_tokens: 500,
       temperature: 0.6,
-      
     });
-    console.log("==================",completion,"++++++++++++++", completion.data.choices.length);
-    console.dir(completion.data.choices[0]);
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
